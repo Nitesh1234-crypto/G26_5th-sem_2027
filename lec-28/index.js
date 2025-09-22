9dd53c333f8ea616626835377a4a236205a31ac1
Nitesh <nitesh.e21@nsut.ac.in>
2025-09-22 13:33:21 +0530

    intialised prisma

---
+++ lec-28/index.js
@@ -0,0 +1,23 @@
+const {PrismaClient} = require('./generated/prisma');
+const prisma = new PrismaClient();
+async function addUser(email,name,password){
+ await prisma.user.create({
+    data:{
+        email:email,
+        name:name,
+        password:password
+    }
+ })
+}
+// addUser("Nites@gmail.com","Ritik","1234")
+// .then(()=>{
+//     console.log("user added successfully!!")
+// })
+async function getAllUser(){
+    let allUser= await prisma.user.findMany();
+    return allUser;
+}
+getAllUser().
+then((data)=>{
+    console.log(data)
+})
